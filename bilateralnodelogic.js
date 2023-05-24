2cf6b181940a7bab5217dc2261cb45c797fdfb8f
glcapps <58761442+glcapps@users.noreply.github.com>
2023-05-24 07:51:26 -0500

    Update bilateralnodelogic.js

---
+++ bilateralnodelogic.js
@@ -0,0 +1,13 @@
+function getCurrentPegWeights() { }
+function randomizePegWeight() { }
+function refinePegWeight(myPeg, myPastProductiveContribution, myPastDestructiveContribution) { }
+function evaluatePegWeight(myPeg) { }
+function getPegExitDistribution(myPeg, myPegEntranceDistribution) { }
+function getPegEntranceDistribution(myPeg, pegWeights) { }
+function getPegExitDistribution(myPeg, pegWeights) { }
+function getBucketEntranceDistribution(myBucket, pegWeights) { }
+function getCurrentRefinementStepSize(myIterationNumber) { }
+function getGravityAdjustedDistribution(myNaturalDistribution) { }
+function getBucketResults(myPegWeights, myInitialBallLocations) { }
+function getIdealBucketResult(myBucket, scenarioInputList, myscenarioPin){}
+function getIterationHappiness(myPegWeights, scenarioInputList, scenarioNeededOutput){}
