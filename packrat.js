3e1316a47f977971f8d6dab7606d5477eacf5ded
glcapps <58761442+glcapps@users.noreply.github.com>
2023-05-24 14:28:56 -0500

    Update packrat.js

---
+++ packrat.js
@@ -0,0 +1,34 @@
+
+            // function launchSubgroup(items) {
+            //     if (medCopyBallAnimations.length > 0) {
+            //         let filteredArray = [];
+            //         let testCircle = myPaper.circle(250, 250, 250).attr({
+            //             fill: colorBrightRed, stroke: colorBlack, strokeWidth: 10
+            //         });
+            //         let testCircleGroup = myPaper.group();
+            //         testCircleGroup.attr({
+            //             transform: 'scale(0.025,0.025)', strokeWidth: 10
+            //         });
+            //         // items[0].append(testCircle);
+            //         testCircleGroup.append(testCircle);
+            //         filteredArray.push(testCircleGroup);
+            //         filteredArray.forEach(function (animationItem) {
+            //             let thisBallGroup = myPaper.group();
+            //             thisBallGroup.append(animationItem);
+            //             let myPath = myPaper.path(bouncePath)
+            //                 .attr({
+            //                     fill: "none", stroke: colorWhite, strokeWidth: 1
+            //                 });
+            //             thisBallGroup.drawAtPath(myPath, 2000, {
+            //                 rotate: true, easing: mina.linear, reverse: false, drawpath: true, callback: function () {
+            //                     //flash bucket
+            //                     // Snap.animate(0, 1, function (value) {
+            //                     //     buckets[6].attr({ fill: value < 1 ? colorWhite : colorMarioGreen });
+            //                     // }, 10);
+            //                     //launchSubgroup();
+            //                 }
+            //             });
+            //         });
+            //     }
+            // }
+            
